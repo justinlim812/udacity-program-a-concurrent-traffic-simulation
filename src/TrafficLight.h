5ee783c4b514cd3e68d5600a072d41b1341521a7
Justin Lim Zhe Shien <zlim029@e.ntu.edu.sg>
2022-10-27 21:49:53 +0800

    Remove unused variable

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -55,7 +55,6 @@ private:
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
     MessageQueue<TrafficLightPhase> _trafficLightPhases;
-    std::condition_variable _condition;
     std::mutex _mutex;
     TrafficLightPhase _currentPhase;
 };
